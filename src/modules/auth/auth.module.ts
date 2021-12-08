215dc57a7c0f9bbcbee74a4a20ea454400a5de09
Jean Junior <jclgjunior@gmail.com>
2021-12-08 13:28:09 -0300

    feat: private routes

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from 'src/modules/auth/controller/rest.controller';
 import { AuthenticateUserService } from 'src/modules/auth/useCases/authenticate-user.service';
+import { JwtStrategyService } from 'src/modules/auth/useCases/jwt-strategy.service';
 
 @Module({
   imports: [
@@ -13,6 +14,6 @@ import { AuthenticateUserService } from 'src/modules/auth/useCases/authenticate-
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthenticateUserService],
+  providers: [AuthenticateUserService, JwtStrategyService],
 })
 export class AuthModule {}
--- src/modules/auth/controller/rest.controller.ts
+++ src/modules/auth/controller/rest.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { RequestLoginDto } from 'src/modules/auth/dto/request-login.dto';
+import { JwtGuard } from 'src/modules/auth/guard/jwt.guard';
 import { AuthenticateUserService } from 'src/modules/auth/useCases/authenticate-user.service';
 
 @Controller('auth')
@@ -7,7 +8,12 @@ export class AuthController {
   constructor(private readonly authenticateUser: AuthenticateUserService) {}
   @Post()
   authenticate(@Body() data: RequestLoginDto) {
-    console.log(data);
     return this.authenticateUser.execute(data);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('ping')
+  test() {
+    return 'pong';
   }
 }
